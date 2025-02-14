4024671524f9867ba32e811836edbc5f30caa3ef
Haqiim <hqcoder.jh@gmail.com>
2025-02-14 10:28:31 +0100

    #

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -9,7 +9,7 @@ export default function Button({clickAction, text, type}) {
 
   return (
     type === "accent"? 
-    <button onClick={(e)=>{action(e)}} type='submit' className='btn py-[12px] rounded-[8px] h-[48px] text-[white] bg-[#24A0B5] px-[24px] w-full border-[#24A0B5] border-[1px]'>{text}</button>:
-    <button onClick={(e)=>{action(e)}} className='btn py-[12px] rounded-[8px] px-[24px] w-full h-[48px] text-[#24A0B5] border-[#24A0B5] border-[1px]'>{text}</button>
+    <button onClick={(e)=>{action(e)}} type='submit' className='btn py-[12px] rounded-[8px] h-[48px] text-[white] hover:bg-[#2C545B] bg-[#24A0B5] px-[24px] w-full border-[#24A0B5] border-[1px]'>{text}</button>:
+    <button onClick={(e)=>{action(e)}} className='btn py-[12px] rounded-[8px] px-[24px] w-full h-[48px] text-[#24A0B5] hover:bg-[#00000030] border-[#24A0B5] border-[1px]'>{text}</button>
   )
 }
